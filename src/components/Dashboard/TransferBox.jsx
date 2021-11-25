f22ae2677a80f075be5114f3f0056958fc7f1325
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-11-25 17:56:50 +0100

    posibilité de choisir le compte débité et le compte crédité

--- src/components/Dashboard/TransferBox.jsx
+++ src/components/Dashboard/TransferBox.jsx
@@ -1,14 +1,58 @@
-import React from 'react'
-import useLocalState from '../../hooks/useLocalState'
+import React from "react"
+
+const defaultAccounts = [
+	{
+		name: "Courant",
+		value: "courant",
+		type: "main",
+	},
+	{
+		name: "epargne",
+		value: "epargne",
+		type: "savings",
+	},
+	{
+		name: "Cryptomonnaie",
+		value: "crypto",
+		type: "savings",
+	},
+]
 
 function TransferBox() {
-  const [accounts, setAccounts] = useLocalState(null, "accounts")
+	const [sender, setSender] = React.useState("")
+	const [receiver, setReceiver] = React.useState("")
 
-  return (
-    <div className="transfer-box">
-      
-    </div>
-  )
+	const handleChangeSender = (event) => {
+		setSender(event.target.value)
+	}
+	const handleChangeReceiver = (event) => {
+		setReceiver(event.target.value)
+	}
+
+	return (
+		<div className="transfer-box">
+			<form>
+				<select onChange={handleChangeSender} value={sender}>
+					{defaultAccounts.map((elm) => {
+						return (
+							<option key={elm.name} value={elm.value}>
+								{elm.name}
+							</option>
+						)
+					})}
+				</select>
+				<select onChange={handleChangeReceiver} disabled={sender == ""} value={receiver}>
+					{defaultAccounts.map((elm) => {
+						return (
+							<option hidden={sender == elm.value} key={elm.name} value={elm.value}>
+								{elm.name}
+							</option>
+						)
+					})}
+				</select>
+			</form>
+		</div>
+	)
 }
 
 export default TransferBox
