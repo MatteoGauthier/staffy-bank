73dfced273d1605bc6f4f9e3d31395cdcfa34c5f
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-12-03 12:30:34 +0100

    tryyy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
 
-// import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [reactRefresh()],
+  build: {
+    outDir: "build"
+  },
 })
