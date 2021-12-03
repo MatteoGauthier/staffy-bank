2f80914856b997d02ea28cc8e03a028f99f67967
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-12-03 12:37:35 +0100

    dd

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
 // import react from '@vitejs/plugin-react'
 
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactRefresh from "@vitejs/plugin-react-refresh"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  build: {
-    outDir: "build"
-  },
+	plugins: [reactRefresh()],
+	build: {
+		outDir: "build",
+	},
 })
